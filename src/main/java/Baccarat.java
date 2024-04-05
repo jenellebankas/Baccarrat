0fdd310216b3bde8ecfe556bcc60a30fa4ea6f1c
leysban023 <ban023@theleys.net>
2024-04-06 00:39:51 +0100

    basic idea for full solution need to clean up though

--- src/main/java/Baccarat.java
+++ src/main/java/Baccarat.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 
 public class Baccarat {
+  public Baccarat() {
+  }
 
   private static void initialDeal(BaccaratHand current, Shoe currentShoe) {
-
     current.cards.add(currentShoe.deal());
-
   }
 
   private static BaccaratHand makeHand() {
-
     return new BaccaratHand();
   }
 
   private static Shoe makeShoe() {
-
     return new Shoe(6);
   }
 
@@ -22,6 +20,10 @@ public class Baccarat {
 
     System.out.printf("Player: %s = %d\n", punto.cards.toString(), punto.value());
     System.out.printf("Banker: %s = %d\n", banco.cards.toString(), banco.value());
+
+  }
+
+  private static void checkNatural(BaccaratHand punto, BaccaratHand banco) {
 
     if(punto.isNatural()) {
       System.out.print("Player has a Natural\n");
@@ -32,46 +34,132 @@ public class Baccarat {
     }
   }
 
-  public static void showResults() {
-
+  private static void finalResult(int numRounds, int playerWins, int bankerWins, int numTies) {
+    System.out.printf("%d rounds played\n", numRounds);
+    System.out.printf("%d player wins\n", playerWins);
+    System.out.printf("%d banker wins\n", bankerWins);
+    System.out.printf("%d ties\n", numTies);
   }
 
+  private static void rules(BaccaratHand punto, BaccaratHand banco, Shoe shoe) {
 
+    if (punto.value() <= 5) {
+      initialDeal(punto, shoe);
+      System.out.println("Dealing third card to player...");
+
+      if (banco.value() <= 2) {
+        initialDeal(banco, shoe);
+      } else if (banco.value() == 3 && punto.cards.getLast().value() == 8) {
+        initialDeal(banco, shoe);
+      } else if (banco.value() == 4 && punto.cards.getLast().value() == 2
+        || punto.cards.getLast().value() == 3 || punto.cards.getLast().value() == 4
+        || punto.cards.getLast().value() == 5 || punto.cards.getLast().value() == 6
+        || punto.cards.getLast().value() == 7) {
+        initialDeal(banco, shoe);
+      } else if (banco.value() == 5 && punto.cards.getLast().value() == 4
+        || punto.cards.getLast().value() == 5 || punto.cards.getLast().value() == 6
+        || punto.cards.getLast().value() == 7) {
+        initialDeal(banco, shoe);
+      } else if (banco.value() == 6 && punto.cards.getLast().value() == 6 ||
+        punto.cards.getLast().value() == 7) {
+        initialDeal(banco, shoe);
+      }
+    }
+  }
+
+  private static int checkWins(BaccaratHand punto, BaccaratHand banco) {
+
+    int puntoDifference = 9 - punto.value();
+    int bancoDifference = 9 - banco.value();
+
+    if (puntoDifference == bancoDifference) {
+      System.out.println("Tie");
+      return 1;
+    } else if (puntoDifference < bancoDifference) {
+      System.out.println("Player win!");
+      return 2;
+    } else {
+      System.out.println("Banker win!");
+      return 3;
+    }
+  }
   public static void main(String[] args) {
 
-    int numRounds = 0;
-    int bancoWins = 0;
+    int numRounds = 1;
+    int bankerWins = 0;
     int playerWins = 0;
     int numTies = 0;
 
-    BaccaratHand punto = makeHand();
-    BaccaratHand banco = makeHand();
+    char userInput = 'y';
 
-    Shoe shoe = makeShoe();
 
-    shoe.shuffle();
+    try {
+      BaccaratHand punto = makeHand();
+      BaccaratHand banco = makeHand();
 
-    initialDeal(punto, shoe);
-    initialDeal(banco, shoe);
-    initialDeal(punto, shoe);
-    initialDeal(banco, shoe);
+      Shoe shoe = makeShoe();
 
-    printResults(punto, banco);
+      shoe.shuffle();
 
-    if (args.length == 0) {
-      while(shoe.size() >= 6) {
-        numRounds++;
+      initialDeal(punto, shoe);
+      initialDeal(banco, shoe);
+      initialDeal(punto, shoe);
+      initialDeal(banco, shoe);
+
+      if (args.length == 0) {
+        while (shoe.size() >= 6) {
+
+          System.out.printf("Round %d\n", numRounds);
+
+          printResults(punto, banco);
+          rules(punto, banco, shoe);
+          checkNatural(punto, banco);
+          if (checkWins(punto, banco) == 1) {
+            numTies++;
+          } else if (checkWins(punto, banco) == 2) {
+            playerWins++;
+          } else {
+            bankerWins++;
+          }
+          numRounds++;
+        }
+      } else if (args.length == 1) {
+        if (args[0].equals("-i") || args[0].equals("--interactive")) {
+
+          while (userInput == 'y' || userInput == 'Y') {
+
+            System.out.printf("Round %d\n", numRounds);
+
+            printResults(punto, banco);
+            rules(punto, banco, shoe);
+            checkNatural(punto, banco);
+            if (checkWins(punto, banco) == 1) {
+              numTies++;
+            } else if (checkWins(punto, banco) == 2) {
+              playerWins++;
+            } else {
+              bankerWins++;
+            }
+            numRounds++;
+
+            System.out.print("Another round? (y/n): ");
+
+            Scanner scanner = new Scanner(System.in);
+            userInput = scanner.next().charAt(0);
+
+            if (userInput == 'n') {
+              break;
+            }
+          }
+        }
       }
-    }
 
-    if(args[1] == "-i" || args[1] == "--interactive") {
-      System.out.println("Another round?");
-      Scanner scanner = new Scanner(System.in);
-      String userInput = scanner.nextLine();
-      while (userInput.equals('y') || userInput.equals('Y')) {
+      finalResult(numRounds, playerWins, bankerWins, numTies);
+      System.exit(0);
 
-        numRounds++;
-      }
+    } catch (CardException cards) {
+      System.err.printf("CardException: %s%n", cards);
+      System.exit(1);
     }
   }
 }
